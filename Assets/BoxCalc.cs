51b7022822db18478c03f0a1ac8fa5fe9e93c45e
Austin <austin.butt@mymail.champlain.edu>
2025-12-08 23:32:59 -0500

    Started Spider Rotation

--- Assets/BoxCalc.cs
+++ Assets/BoxCalc.cs
@@ -15,74 +15,104 @@ public class BoxCalc : MonoBehaviour
     }
 
     [SerializeField] private List<Harch> arrTargets;
-    [Range(0,1)]
+    [Range(0, 1)]
     [SerializeField] public float snapDistance = 0.57f;
     [Range(0, 1)]
     [SerializeField] public float legSmoothing = 0.4f;
 
     private bool alternateLegCall = false;
+    private Vector3 lastBodyUp;
+
+    private void Start()
+    {
+        lastBodyUp = transform.up;
+    }
 
     private void Update()
     {
         CalcDistance();
 
-        if(alternateLegCall)
+        if (alternateLegCall)
         {
             handleOddLeggs();
         }
+
+        //rotateBody();
     }
 
     private void CalcDistance()
     {
-        for(int i = 0; i < arrTargets.Count; i++)
+        for (int i = 0; i < arrTargets.Count; i++)
         {
             Vector3 dif = arrTargets[i].target.position - arrTargets[i].tracker.transform.position;
-            if (MathF.Abs(dif.x) > snapDistance || 
-                MathF.Abs(dif.y) > snapDistance || 
+
+            //if distance is too far, snap
+            if (MathF.Abs(dif.x) > snapDistance ||
+                MathF.Abs(dif.y) > snapDistance ||
                 MathF.Abs(dif.z) > snapDistance) //   :(
             {
 
-                int otherLegIndex = i + 2;
-                if (otherLegIndex > 3)
+                if (i == 0 || i == 1)
                 {
-                    otherLegIndex = otherLegIndex - 4;
-                }
-                if(i == 0 || i == 1)
-                {
+                    //calls two pairs of leggs
                     StartCoroutine(LerpLeg(arrTargets[i].target.position, arrTargets[i].tracker.transform.position, i, true));
                 }
 
-                //StartCoroutine(LerpLeg(arrTargets[otherLegIndex].target.position, arrTargets[otherLegIndex].tracker.transform.position, otherLegIndex));
+            }
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(arrTargets[i].tracker.transform.position, Vector3.down, out hit, 10))
+            {
+                Debug.DrawRay(arrTargets[i].tracker.transform.position, Vector3.down * hit.distance, Color.green);
             }
         }
     }
 
     private void handleOddLeggs()
     {
-        StartCoroutine(LerpLeg(arrTargets[2].target.position, arrTargets[2].tracker.transform.position, 2, false));
-        StartCoroutine(LerpLeg(arrTargets[3].target.position, arrTargets[3].tracker.transform.position, 3, false));
+        for (int i = 2; i < 4; i++)
+        {
+            //handles movement for offlegs simulating spider movement
+            StartCoroutine(LerpLeg(arrTargets[2].target.position, arrTargets[2].tracker.transform.position, 2, true));
+            StartCoroutine(LerpLeg(arrTargets[3].target.position, arrTargets[3].tracker.transform.position, 3, false));
+            alternateLegCall = false;
+        }
 
     }
 
+    //smooths the leg movement to the location
     IEnumerator LerpLeg(Vector3 tar, Vector3 tracker, int index, bool callLeg)
     {
 
         float totalTime = 0;
 
-        while(totalTime < legSmoothing)
+        while (totalTime < legSmoothing)
         {
             float t = totalTime / legSmoothing;
-            
-            arrTargets[index].target.transform.position = BlendNodes.LerpVec3(tar, tracker + new Vector3 (0,MathF.Sin(t *MathF.PI) * 0.2f, 0), t);
+
+            arrTargets[index].target.transform.position = BlendNodes.LerpVec3(tar, tracker + new Vector3(0, MathF.Sin(t * MathF.PI) * 0.2f, 0), t);
 
             totalTime += Time.deltaTime;
 
             yield return null;
         }
 
-        
+
         arrTargets[index].target.transform.position = tracker;
         alternateLegCall = callLeg;
-        
+
+    }
+
+    void rotateBody()
+    {
+
+        Vector3 v1 = arrTargets[0].target.position - arrTargets[1].target.position;
+        Vector3 v2 = arrTargets[2].target.position - arrTargets[3].target.position;
+        Vector3 normal = Vector3.Cross(v1, v2).normalized;
+        Vector3 up = Vector3.Lerp(lastBodyUp, normal, 1f / (float)(8));
+        transform.up = up;
+        transform.rotation = Quaternion.LookRotation(transform.parent.forward, up);
+        lastBodyUp = transform.up;
     }
 }
